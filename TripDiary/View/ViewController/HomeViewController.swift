7eaf221edc56149600b711618a08b7b8410c739a
taeuk <nous178@naver.com>
2021-03-26 12:42:02 +0900

    Feat: Home화면 생성

--- TripDiary/View/ViewController/HomeViewController.swift
+++ TripDiary/View/ViewController/HomeViewController.swift
@@ -9,10 +9,47 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        
     }
+}
 
+extension HomeViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 220
+    }
+    
+}
+
+extension HomeViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "myTravel", for: indexPath) as? HomeViewControllerCell else { return UITableViewCell() }
+        cell.locateName.text = "남이섬"
+        cell.travelPeriod.text = "8-20 PM 02:00"
+        
+        cell.travelPicture.sizeToFit()
+        cell.travelPicture.image = UIImage(named: "yeosu")
+        cell.travelPicture.layer.cornerRadius = 15
+        return cell
+    }
+    
+}
+
+class HomeViewControllerCell: UITableViewCell {
+    
+    @IBOutlet weak var locateName: UILabel!
+    @IBOutlet weak var travelPeriod: UILabel!
+    @IBOutlet weak var travelPicture: UIImageView!
+    
 }
--- TripDiary/ViewModel/SignUpViewModel.swift
+++ TripDiary/ViewModel/SignUpViewModel.swift
@@ -7,26 +7,25 @@
 
 import Foundation
 import Alamofire
+import AuthenticationServices
 
 struct SignUpViewModel {
     
-    var signUpRequeset: SignUpModel?
     
-//    init(email: String, password: String) {
-//        requestSignUp(email: email, password: password)
-//    }
     init () {}
     
     func requestSignUp(email: String, password: String) {
         let signUpAPI = SignUpModel(email: email, password: password, provider: "N", token: "test", nickname: email)
         guard let uploadData = try? JSONEncoder().encode(signUpAPI) else { return }
         print(uploadData)
+        
         guard let url = URL(string: "https://azanghs.cafe24.com/itstudy/signup.php") else { return }
+        
         
         var request = URLRequest(url: url)
         request.httpMethod = HTTPMethod.post.rawValue
         request.setValue("application/json", forHTTPHeaderField: "Content-type")
-        
+        print(request)
         let task = URLSession.shared.uploadTask(with: request, from: uploadData) { (data, response, err) in
             if let err = err{
                 print(err.localizedDescription)
